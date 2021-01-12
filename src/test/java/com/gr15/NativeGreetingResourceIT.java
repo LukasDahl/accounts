fb348ed6aa64aadeaaeaaa68875135e4cb496cd3
Jonatan <s185096@student.dtu.dk>
2021-01-12 16:00:19 +0100

    Cucumber added as a dependency.

--- src/test/java/com/gr15/NativeGreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.gr15;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
