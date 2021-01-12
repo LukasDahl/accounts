64fe91c4ea889c2ec91dcd74e6dbd08200826d84
Jonatan <s185096@student.dtu.dk>
2021-01-12 14:49:47 +0100

    Added dummy data.

--- src/main/java/com/gr15/RestCom.java
+++ src/main/java/com/gr15/RestCom.java
@@ -1,13 +1,24 @@
 package com.gr15;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/hello-resteasy")
 public class RestCom {
+    private List<User> dummyUsers = new ArrayList<>();
+
+    public RestCom(){
+        dummyUsers.add(new User("000000-0000", "Jonatan", "Jonatansen",
+                new Account("0", "costumer", "10")));
+        dummyUsers.add(new User("000000-0001", "Hans", "Hansen",
+                new Account("0", "costumer", "11")));
+        dummyUsers.add(new User("000000-0002", "Micheal", "Jordan",
+                new Account("0", "merchant", "12")));
+        dummyUsers.add(new User("000000-0003", "August", "Augustsen",
+                new Account("0", "costumer", "13")));
+    }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
@@ -20,5 +31,15 @@ public class RestCom {
 
 
         return "Hello RESTEasy";
+    }
+
+    @DELETE
+    public String deleteUser(@PathParam(MediaType.APPLICATION_JSON) String userId) {
+
+
+
+
+
+        return "200";
     }
 }
