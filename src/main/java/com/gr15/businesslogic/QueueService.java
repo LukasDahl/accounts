a1b46c18d60efa921ef6bb7c7e0ec47eb8cb9df5
augus <august2macholm@gmail.com>
2021-01-16 11:29:34 +0100

    remove not used stuff

--- src/main/java/com/gr15/businesslogic/QueueService.java
+++ src/main/java/com/gr15/businesslogic/QueueService.java
@@ -7,41 +7,31 @@ package com.gr15.businesslogic;
 import com.google.gson.Gson;
 import com.gr15.businesslogic.exceptions.QueueException;
 import com.gr15.businesslogic.model.Account;
-import com.gr15.businesslogic.model.TokenInfo;
 import com.gr15.messaging.interfaces.IEventReceiver;
 import com.gr15.messaging.interfaces.IEventSender;
 import com.gr15.messaging.models.Event;
 
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 
 public class QueueService implements IEventReceiver, IQueueService {
 
     private static final String QUEUE_TYPE = "topic";
     private static final String EXCHANGE_NAME = "accountsExchange";
 
-    private static final String TOKEN_CMD_BASE = "token.cmds.";
-    private static final String ACCOUNT_CMD_BASE = "account.cmds.";
-    private static final String TRANSACTION_EVENT_BASE = "transaction.events.";
     private static final String ACCOUNT_EVENT_BASE = "account.events";
 
-    private static final String VALIDATE_TOKEN_CMD = "validateToken";
-    private static final String VALIDATE_ACCOUNT_CMD = "validateAccount";
+
     private static final String VALIDATE_ACCOUNTS_CMD = "validateAccounts";
 
     private static final String ACCOUNT_EXISTS_CMD = "accountExistsRequest";
     private static final String ACCOUNT_EXISTS_EVENT = "accountExistsResponse";
 
-    private static final String TOKEN_VALIDATED_EVENT = "tokenValidated";
     private static final String ACCOUNT_VALIDATED_EVENT = "accountValidated";
-    //private static final String ACCOUNTS_VALIDATED_EVENT = "accountsValidated";
-    private static final String TRANSACTION_CREATED_EVENT = "transactionCreated";
 
     private static final String ACCOUNT_DELETE_EVENT = "accountDeleted";
 
     private IEventSender eventSender;
-    private CompletableFuture<Account> accountResult;
-    private CompletableFuture<TokenInfo> tokenInfoResult;
+
     private AccountManager accountManager;
 
     public QueueService(IEventSender eventSender) {
